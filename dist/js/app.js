0e147180b3f995bbc86e0a8d99a856d868d07880
Elizavet-8 <businessgrazion@gmail.com>
2022-05-04 02:22:08 +0300

    functional

--- dist/js/app.js
+++ dist/js/app.js
@@ -26,21 +26,173 @@ document.addEventListener("DOMContentLoaded", function () {
       scrollTop: top
     }, 1500);
   }); //модалки
-  // $(document).ready(function () {
-  //     $("#application-modal").wgModal({
-  //         triggerElement: '#application-open-modal',
-  //         theme: "info"
-  //     });
-  // });
-  //форма заявки
-  //телефон
 
-  function phoneMask() {
-    var num = $(this).val().replace(/\D/g, '');
-    $(this).val('+7 (' + num.substring(0, 3) + ') ' + num.substring(3, 6) + '-' + num.substring(6, 8) + '-' + num.substring(8, 10));
-  }
+  $("#application-modal").wgModal({
+    triggerElement: '.application-open-modal',
+    onBeforeOpen: function onBeforeOpen(e) {
+      $('body').toggleClass('overflow');
+    },
+    onBeforeClose: function onBeforeClose(e) {
+      $('body').removeClass('overflow');
+    }
+  });
+  $("#opportunities-modal").wgModal({
+    triggerElement: '.opportunities-open-modal',
+    onBeforeOpen: function onBeforeOpen(e) {
+      $('body').toggleClass('overflow');
+    },
+    onBeforeClose: function onBeforeClose(e) {
+      $('body').removeClass('overflow');
+    }
+  });
+  $("#success-modal").wgModal({
+    onBeforeOpen: function onBeforeOpen(e) {
+      $('body').toggleClass('overflow');
+    },
+    onBeforeClose: function onBeforeClose(e) {
+      $('body').removeClass('overflow');
+    }
+  });
+  $("#error-modal").wgModal({
+    onBeforeOpen: function onBeforeOpen(e) {
+      $('body').toggleClass('overflow');
+    },
+    onBeforeClose: function onBeforeClose(e) {
+      $('body').removeClass('overflow');
+    }
+  }); //телефон
 
-  $('[type="tel"]').keyup(phoneMask); //аккардион
+  window.addEventListener("DOMContentLoaded", function () {
+    [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+      var keyCode;
+
+      function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7 (___) ___-__-__",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+        });
+        i = new_value.indexOf("_");
+
+        if (i != -1) {
+          i < 5 && (i = 3);
+          new_value = new_value.slice(0, i);
+        }
+
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g, function (a) {
+          return "\\d{1," + a.length + "}";
+        }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5) this.value = "";
+      }
+
+      input.addEventListener("input", mask, false);
+      input.addEventListener("focus", mask, false);
+      input.addEventListener("blur", mask, false);
+      input.addEventListener("keydown", mask, false);
+    });
+  }); //валидация
+
+  $("#application-form").validate({
+    rules: {
+      name: {
+        required: true
+      },
+      phone: {
+        required: true,
+        minlength: 16
+      },
+      email: {
+        email: true
+      }
+    },
+    messages: {
+      name: {
+        required: 'Поле обязательно для заполнения'
+      },
+      phone: {
+        required: 'Поле обязательно для заполнения',
+        minlength: 'Введите номер в формате "+7 (123) 456-78-90"'
+      },
+      email: {
+        email: 'Введите корректный email'
+      }
+    },
+    submitHandler: function submitHandler() {
+      var modalClose = $("#application-modal").wgModal();
+      $('#application-modal')(function () {
+        modalClose.closeModal();
+      });
+      var modal = $("#success-modal").wgModal();
+      $('#success-modal')(function () {
+        modal.openModal();
+      });
+    }
+  });
+  $("#opportunities-form").validate({
+    rules: {
+      name: {
+        required: true
+      },
+      phone: {
+        required: true,
+        minlength: 16
+      },
+      email: {
+        email: true
+      }
+    },
+    messages: {
+      name: {
+        required: 'Поле обязательно для заполнения'
+      },
+      phone: {
+        required: 'Поле обязательно для заполнения',
+        minlength: 'Введите номер в формате "+7 (123) 456-78-90"'
+      },
+      email: {
+        email: 'Введите корректный email'
+      }
+    }
+  });
+  $("#questions-form").validate({
+    rules: {
+      name: {
+        required: true
+      },
+      phone: {
+        required: true,
+        minlength: 16
+      },
+      email: {
+        email: true
+      },
+      city: {
+        required: true
+      }
+    },
+    messages: {
+      name: {
+        required: 'Поле обязательно для заполнения'
+      },
+      phone: {
+        required: 'Поле обязательно для заполнения',
+        minlength: 'Введите номер в формате "+7 (123) 456-78-90"'
+      },
+      email: {
+        email: 'Введите корректный email'
+      },
+      city: {
+        required: 'Поле обязательно для заполнения'
+      }
+    }
+  }); //аккардион
 
   $(function () {
     // (Optional) Active an item if it has the class "active"
--- src/js/app.js
+++ src/js/app.js
@@ -18,30 +18,186 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     //модалки
-    // $(document).ready(function () {
-    //     $("#application-modal").wgModal({
-    //         triggerElement: '#application-open-modal',
-    //         theme: "info"
-    //     });
-    // });
+    $("#application-modal").wgModal({
+        triggerElement: '.application-open-modal',
+        onBeforeOpen: function (e) {
+            $('body').toggleClass('overflow');
+        },
+        onBeforeClose: function (e) {
+            $('body').removeClass('overflow');
+        },
+    });
+    $("#opportunities-modal").wgModal({
+        triggerElement: '.opportunities-open-modal',
+        onBeforeOpen: function (e) {
+            $('body').toggleClass('overflow');
+        },
+        onBeforeClose: function (e) {
+            $('body').removeClass('overflow');
+        },
+    });
+    $("#success-modal").wgModal({
+        onBeforeOpen: function (e) {
+            $('body').toggleClass('overflow');
+        },
+        onBeforeClose: function (e) {
+            $('body').removeClass('overflow');
+        },
+    });
+    $("#error-modal").wgModal({
+        onBeforeOpen: function (e) {
+            $('body').toggleClass('overflow');
+        },
+        onBeforeClose: function (e) {
+            $('body').removeClass('overflow');
+        },
+    });
 
-    //форма заявки
 
     //телефон
-    function phoneMask() {
-        var num = $(this).val().replace(/\D/g, '');
-        $(this).val('+7 (' + num.substring(0, 3) + ') ' + num.substring(3, 6) + '-' + num.substring(6, 8) + '-' + num.substring(8, 10));
-    }
+    window.addEventListener("DOMContentLoaded", function () {
+        [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+            var keyCode;
 
-    $('[type="tel"]').keyup(phoneMask);
+            function mask(event) {
+                event.keyCode && (keyCode = event.keyCode);
+                var pos = this.selectionStart;
+                if (pos < 3) event.preventDefault();
+                var matrix = "+7 (___) ___-__-__",
+                    i = 0,
+                    def = matrix.replace(/\D/g, ""),
+                    val = this.value.replace(/\D/g, ""),
+                    new_value = matrix.replace(/[_\d]/g, function (a) {
+                        return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                    });
+                i = new_value.indexOf("_");
+                if (i != -1) {
+                    i < 5 && (i = 3);
+                    new_value = new_value.slice(0, i)
+                }
+                var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                    function (a) {
+                        return "\\d{1," + a.length + "}"
+                    }).replace(/[+()]/g, "\\$&");
+                reg = new RegExp("^" + reg + "$");
+                if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+                if (event.type == "blur" && this.value.length < 5) this.value = ""
+            }
+
+            input.addEventListener("input", mask, false);
+            input.addEventListener("focus", mask, false);
+            input.addEventListener("blur", mask, false);
+            input.addEventListener("keydown", mask, false)
+
+        });
+
+    });
+
+    //валидация
+    $("#application-form").validate({
+        rules: {
+            name: {
+                required: true,
+            },
+            phone: {
+                required: true,
+                minlength: 16
+            },
+            email: {
+                email: true
+            },
+        },
+        messages: {
+            name: {
+                required: 'Поле обязательно для заполнения',
+            },
+            phone: {
+                required: 'Поле обязательно для заполнения',
+                minlength: 'Введите номер в формате "+7 (123) 456-78-90"'
+            },
+            email: {
+                email: 'Введите корректный email'
+            },
+        },
+        submitHandler: function () {
+            let modalClose = $("#application-modal").wgModal();
+            $('#application-modal')(function () {
+                modalClose.closeModal();
+            });
+
+            let modal = $("#success-modal").wgModal();
+
+            $('#success-modal')(function () {
+                modal.openModal();
+            });
+        }
+    });
+    $("#opportunities-form").validate({
+        rules: {
+            name: {
+                required: true,
+            },
+            phone: {
+                required: true,
+                minlength: 16
+            },
+            email: {
+                email: true
+            },
+        },
+        messages: {
+            name: {
+                required: 'Поле обязательно для заполнения',
+            },
+            phone: {
+                required: 'Поле обязательно для заполнения',
+                minlength: 'Введите номер в формате "+7 (123) 456-78-90"'
+            },
+            email: {
+                email: 'Введите корректный email'
+            },
+        }
+    });
+    $("#questions-form").validate({
+        rules: {
+            name: {
+                required: true,
+            },
+            phone: {
+                required: true,
+                minlength: 16
+            },
+            email: {
+                email: true
+            },
+            city: {
+                required: true,
+            },
+        },
+        messages: {
+            name: {
+                required: 'Поле обязательно для заполнения',
+            },
+            phone: {
+                required: 'Поле обязательно для заполнения',
+                minlength: 'Введите номер в формате "+7 (123) 456-78-90"'
+            },
+            email: {
+                email: 'Введите корректный email'
+            },
+            city: {
+                required: 'Поле обязательно для заполнения',
+            },
+        }
+    });
 
     //аккардион
 
-    $(function() {
+    $(function () {
         // (Optional) Active an item if it has the class "active"
         $(".materials-accordion__item.active").children(".materials-accordion__body").slideDown();
 
-        $(".materials-accordion__item").click(function() {
+        $(".materials-accordion__item").click(function () {
             // Cancel the siblings
             $(this).siblings(".materials-accordion__item").removeClass("active").children(".materials-accordion__body").slideUp();
             // Toggle the item
